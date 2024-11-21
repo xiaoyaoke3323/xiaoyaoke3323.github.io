c34e2245293fb5c5f178ab559aa6935e0ed580d5
Vincent Wang(王巍) <18874893323@163.com>
2024-11-21 11:17:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Personal Resume Boilerplate
+
+I make this boilerplate template out of my personal resume, currently built with Vue.js, but I intend to create alternative versions with vanilla HTML, CSS, JavaScript and other tech stacks so it is more accessible to developers.
+
+## Versions
+
+- HTML, CSS & JavaScript: @TODO
+- Astro: @TODO
+- React: @TODO
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+pnpm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+pnpm dev
+```
+
+### Compile and Minify for Production
+
+```sh
+pnpm build
+```
---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+// Components
+import ResumeSection from "./components/ResumeSection.vue";
+import Highlights from "./components/Highlights.vue";
+import Experiences from "./components/Experiences.vue";
+import Certificates from "./components/Certificates.vue";
+import Projects from "./components/Projects.vue";
+import Educations from "./components/Educations.vue";
+import ResumeHeader from "./components/ResumeHeader.vue";
+
+// Data
+import projects from "./data/projects.json";
+import educations from "./data/educations.json";
+import highlights from "./data/highlights.json";
+import experiences from "./data/experiences.json";
+import certificates from "./data/certificates.json";
+import authorImage from "./assets/author.jpg";
+import introduction from "./data/introduction.json";
+</script>
+
+<template>
+  <ResumeHeader :name="introduction.name" :authorImage="authorImage" :description="introduction.description" />
+
+  <ResumeSection heading="Highlights">
+    <Highlights :highlights="highlights" />
+  </ResumeSection>
+
+  <ResumeSection heading="Experiences">
+    <Experiences :experiences="experiences" />
+  </ResumeSection>
+
+  <ResumeSection heading="Certificates">
+    <Certificates :certificates="certificates" />
+  </ResumeSection>
+
+  <ResumeSection heading="Projects">
+    <Projects :projects="projects" />
+  </ResumeSection>
+
+  <ResumeSection heading="Education">
+    <Educations :educations="educations" />
+  </ResumeSection>
+</template>
+
+<style scoped>
+</style>
